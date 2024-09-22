bcf6ffa294959f369f1986da72f010335bff4462
Alejandro <hgaviria@cesde.net>
2024-09-22 12:31:28 -0500

    BaseDatos.js con los 20 ejemplos gatos diarios

---
+++ src/controllers/BaseDatos.js
@@ -0,0 +1,125 @@
+let gastosDos=Array(
+   {
+        id:1,
+        descripcion:"Pasaje de sabaneta a cesde",
+        monto:8000,
+        fecha:"2024-09-20"
+
+    },
+    {
+        id:2,
+        descripcion:"Empanada de pollo + coca cola",
+        monto:10000,
+        fecha:"2024-09-20"
+    
+    }
+    {
+        id:3,
+        descripcion:"Servcios públicos",
+        monto:247000,
+        fecha:"2024-09-22"   
+    }
+    {
+        id:4,
+        descripcion:"Servcios Teleocmunicaciones",
+        monto:86247,
+        fecha:"2024-09-22"   
+    }
+    {
+        id:5,
+        descripcion:"Tarjeta de credito",
+        monto:136000,
+        fecha:"2024-09-18"   
+    }
+    {
+        id:6,
+        descripcion:"Alimentacion",
+        monto:600000,
+        fecha:"2024-09-15"   
+    }
+    {
+        id:7,
+        descripcion:"vestuario",
+        monto:350000,
+        fecha:"2024-09-19"   
+    }
+    {
+        id:8,
+        descripcion:"Diversion",
+        monto:185000,
+        fecha:"2024-09-21"   
+    }
+    {
+        id:9,
+        descripcion:"Administración",
+        monto:174650,
+        fecha:"2024-08-30"   
+    }
+    {
+        id:10,
+        descripcion:"Transporte",
+        monto:162000,
+        fecha:"2024-09-15"   
+    }
+    {
+        id:11,
+        descripcion:"Gastos varios",
+        monto:36000,
+        fecha:"2024-09-19"   
+    }
+    {
+        id:12,
+        descripcion:"Plan postpago",
+        monto:17450,
+        fecha:"2024-08-30"   
+    }
+    {
+        id:13,
+        descripcion:"Parqueadero moto",
+        monto:40000,
+        fecha:"2024-09-17"   
+    }
+    {
+        id:14,
+        descripcion:"Papeleria",
+        monto:13000,
+        fecha:"2024-09-19"   
+    }
+    {
+        id:15,
+        descripcion:"Creditos almacenes",
+        monto:125000,
+        fecha:"2024-09-15"   
+    }
+    {
+        id:16,
+        descripcion:"Compra SSD Y diadema",
+        monto:350000,
+        fecha:"2024-08-15"   
+    }
+    {
+        id:17,
+        descripcion:"Medicamentos",
+        monto:18000,
+        fecha:"2024-09-19"   
+    }
+    {
+        id:18,
+        descripcion:"Cuota EPS",
+        monto:4550,
+        fecha:"2024-08-10"   
+    }
+    {
+        id:19,
+        descripcion:"Frutas",
+        monto:5500,
+        fecha:"2024-08_01"   
+    }
+    {
+        id:20,
+        descripcion:"cuota manejo tarjeta",
+        monto:4100,
+        fecha:"2024-08-30"   
+    }
+
+)
